03e1b994b6480722bfe25d1756742f61b1975a39
YeongJae0114 <yjl8628@naver.com>
2023-03-13 15:18:29 +0900

    main

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import cmodel
+from .forms import cform
 
 def home(request):
     return render(request, 'home.html')
 
 def new(request):
     return render(request, 'new.html')
+
+    
+
+def createmf(request):
+    if request.method =='POST':
+        form = cform(request.POST)
+        if form.is_valid(): #데이터 유효성 검사
+            form.save()
+            return redirect('home')
+    else:
+        form = cform()
+    return render(request, 'new.html', {'form':form})    
